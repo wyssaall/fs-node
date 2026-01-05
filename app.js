d8c4ae232d3196b4b307ab3714b53f9e7cc976a8
Wissal <wissal1zabour@gmail.com>
2026-01-05 14:36:01 +0100

    deleting file

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 import fs from "fs";
+import { console } from "inspector";
 
-fs.writeFile('./filetest.txt', 'testing', 'utf8', (err, data)=>{
+// fs.writeFile('./filetest.txt', 'testing', 'utf8', (err, data)=>{
+//     if(err){
+//         console.log('error');
+//     }else{
+//         console.log('success');
+//     }
+// })
+
+
+// fs.readFile('./filetest.txt', 'utf8', (err, data)=>{
+//     if(err){
+//         console.log('error');
+//     }else{
+//         console.log(data);
+//     }
+// })
+
+
+fs.unlink('./filetest.txt', (err)=>{
     if(err){
         console.log('error');
     }else{
         console.log('success');
-    }
-})
-
-
-fs.readFile('./filetest.txt', 'utf8', (err, data)=>{
-    if(err){
-        console.log('error');
-    }else{
-        console.log(data);
     }
 })
