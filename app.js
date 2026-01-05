eaf0f2fe1a279783fb4101d76f4646e24748b95b
Wissal <wissal1zabour@gmail.com>
2026-01-05 14:32:52 +0100

    reading from the file

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import fs from "fs";
 
-fs.writeFile('./file.txt', 'creating and writhing this in the txt file', 'utf8', (err, data)=>{
+fs.writeFile('./filetest.txt', 'testing', 'utf8', (err, data)=>{
     if(err){
         console.log('error');
     }else{
         console.log('success');
+    }
+})
+
+
+fs.readFile('./filetest.txt', 'utf8', (err, data)=>{
+    if(err){
+        console.log('error');
+    }else{
+        console.log(data);
     }
 })
